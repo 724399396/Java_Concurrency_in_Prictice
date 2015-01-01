d6ea9e619e4130d0c167e184a41e68782c3fc26b
Li Wei <724399396@qq.com>
2015-01-01 14:10:09 +0800

    2015.1.1 from ubuntu finish ch12.

---
+++ concurrency/src/concurrency/test/TestTrheadPool.java
@@ -0,0 +1,42 @@
+package concurrency.test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+
+import junit.framework.TestCase;
+
+public class TestTrheadPool extends TestCase {
+	private final TestingThreadFactory threadFactory = new TestingThreadFactory();
+	
+	public void testPoolExpansion() throws InterruptedException {
+		int MAX_SIZE = 10;
+		ExecutorService exec = Executors.newFixedThreadPool(MAX_SIZE, threadFactory);
+		
+		for (int i = 0; i < 10 * MAX_SIZE; i++) 
+			exec.execute(new Runnable() {
+				public void run() {
+					try {
+						Thread.sleep(Long.MAX_VALUE);
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
+				}
+			});
+		for (int i = 0;
+				i < 20 && threadFactory.numCreated.get() < MAX_SIZE;
+				i++ ) 
+			Thread.sleep(100);
+		assertEquals(threadFactory.numCreated.get(),MAX_SIZE);
+		exec.shutdownNow();
+	}
+	
+	class TestingThreadFactory implements ThreadFactory {
+		public final AtomicInteger numCreated = new AtomicInteger();
+		private final ThreadFactory factory = Executors.defaultThreadFactory();
+		
+		public Thread newThread(Runnable r) {
+			numCreated.incrementAndGet();
+			return factory.newThread(r);
+		}
+	}
+}
--- concurrency/src/performance/scalable/BetterAtttributeStore.java
+++ concurrency/src/performance/scalable/BetterAtttributeStore.java
@@ -1,5 +1,24 @@
 package performance.scalable;
 
-public class BetterAtttributeStore {
+import net.jcip.annotations.*;
 
+import java.util.*;
+import java.util.regex.Pattern;
+
+@ThreadSafe
+public class BetterAtttributeStore {
+	@GuardedBy("this") private final Map<String, String> attributes
+		= new HashMap<String, String>();
+	
+	public boolean userLocationMatches(String name, String regexp) {
+		String key = "users." + name + ".location";
+		String location;
+		synchronized (this) {
+			location = attributes.get(key);
+		}
+		if (location == null)
+			return false;
+		else
+			return Pattern.matches(regexp, location);
+	}
 }
